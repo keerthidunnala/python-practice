535b7c37517fe6c2853772e7ae61ad70c18c2dd1
keerthidunnala <93655743+keerthidunnala@users.noreply.github.com>
2022-01-04 10:28:50 +0530

    Add files via upload

---
+++ square root.py
@@ -0,0 +1,13 @@
+
+#7. Write a Python program to find the Square root of the given Number
+
+#Input: 8
+#Output: 2.828
+
+#Input: 15
+#Output: 3.873
+
+#Input: 25.83
+import math
+a=int(input('a:'))
+print(math.sqrt(a))
