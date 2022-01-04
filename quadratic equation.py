bfad1c7a768a39b8d7340822251e1dfd05015966
keerthidunnala <93655743+keerthidunnala@users.noreply.github.com>
2022-01-04 10:13:54 +0530

    Add files via upload

---
+++ quadratic equation.py
@@ -0,0 +1,14 @@
+#8: Write a Python program to solve the Quadratic Equation
+#Formula:
+#ax2 + bx + c = 0, where
+#a, b and c are real numbers and
+#a ≠ 0
+
+import cmath
+a=1
+b=5
+c=6
+d = (b**2) - (4*a*c)
+sol1 = (-b-cmath.sqrt(d))/(2*a)
+sol2 = (-b+cmath.sqrt(d))/(2*a)
+print('The solution are {0} and {1}'.format(sol1,sol2))
