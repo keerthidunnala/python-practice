dab56d179f550bbeaf227a33b534c9df094277bb
keerthidunnala <93655743+keerthidunnala@users.noreply.github.com>
2022-01-04 09:54:43 +0530

    Add files via upload

---
+++ 1.1.py
@@ -0,0 +1,14 @@
+#String
+string = 'abcdefghi'
+length = len(string)
+length = length//2 -1
+print(string[length:length+3])
+str1 = 'James'
+print("Original String is", str1)
+res = str1[0]
+l = len(str1)
+cal = int(l / 2)
+result = result + str1[cal]
+result = result + str1[l - 1]
+
+print("New String:", result)
